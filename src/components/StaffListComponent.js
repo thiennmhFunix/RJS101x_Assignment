8bfd00591268b3418ed5c3fa7d22152f277175a1
nmht <thiennmhFX15190@funix.edu.vn>
2022-04-03 14:46:27 +0700

    Finish Assignment 1

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,5 +1,6 @@
 import React, { Component} from "react";
 import { Card, CardText, CardTitle } from 'reactstrap';
+import dateFormat from 'dateformat';
 
 class StaffList extends Component {
 
@@ -7,12 +8,50 @@ class StaffList extends Component {
         super(props);
 
         this.state = {
-            selectedStaff: null
+            selectedStaff: null,
+            btnMode: 1
         }
     }
 
     onStaffSelect(staff) {
         this.setState({ selectedStaff: staff})
+    }
+
+    onBtnClick(mode) {
+        switch (mode) {
+            case 1:
+                this.setState({ btnMode: 2});
+                break;
+            case 2:
+                this.setState({ btnMode: 3});
+                break;
+            case 3:
+                this.setState({ btnMode: 1});
+                break;
+        }
+            
+    }
+
+    settingButton() {
+        return (
+            <div width="100%">
+                <div className="container-btn">
+                    <i class="fa-solid fa-gear" onClick={() => this.onBtnClick(this.state.btnMode)}></i>
+                </div>
+            </div>
+        )
+    }
+
+    modeInterface(mode) {
+        if (mode == 1) {
+            return ("col-12 col-sm-6 col-md-4 my-1");
+        }
+        else if (mode == 2) {
+            return ("col-12 col-sm-6 col-md-2 my-1");
+        }
+        else  {
+            return ("col-12 col-sm-6 col-md-6 my-1");
+        }
     }
 
     renderStaff(staff) {
@@ -21,8 +60,8 @@ class StaffList extends Component {
                 <Card>
                     <CardTitle>Họ và tên: {staff.name}</CardTitle>
                     
-                        <CardText>Ngày sinh: {staff.doB}</CardText>
-                        <CardText>Ngày vào công ty: {staff.startDate}</CardText>
+                        <CardText>Ngày sinh: {dateFormat(staff.doB, "dd/mm/yyyy")}</CardText>
+                        <CardText>Ngày vào công ty: {dateFormat(staff.startDate, "dd/mm/yyyy")}</CardText>
                         <CardText>Phòng ban: {staff.department.name}</CardText>
                         <CardText>Số ngày nghỉ còn lại: {staff.annualLeave}</CardText>
                         <CardText>Số ngày đã làm thêm: {staff.overTime}</CardText>    
@@ -31,7 +70,7 @@ class StaffList extends Component {
         }
         else {
             return (
-                <div></div>
+                <div><p>Bấm vào tên nhân viên để xem thông tin.</p></div>
             );
         }
     }
@@ -40,7 +79,7 @@ class StaffList extends Component {
 
         const stafflist = this.props.staffs.map((staff) => {
             return (
-                <div key={staff.id} className="col-12 col-md-5 m-1">
+                <div key={staff.id} className={this.modeInterface(this.state.btnMode)}>
                     <Card onClick={() => this.onStaffSelect(staff)}>
                         <CardText>{staff.name}</CardText>
                     </Card>
@@ -51,13 +90,13 @@ class StaffList extends Component {
         return (
             <div className="container">
                 <div className="row">
+                    {this.settingButton()}
                     {stafflist}
                 </div>
                 <div className="row">
                     {this.renderStaff(this.state.selectedStaff)}
                 </div>
             </div>
-
         );    
 
     }
